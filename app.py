d4c099d4997eee7a754acd1fc7708e81ab7d5318
endalk <endalkmakonnen@gmail.com>
2023-02-06 19:57:51 -0500

    Added new route for hello HTML page

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Endalk!'
+    return 'Hello World from Endalkk!'
 #helloo
 
 @app.route('/hello')
