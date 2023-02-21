799d2ed43e4e3bf03255c7cc3961f0966ef8a81c
endalk <endalkmakonnen@gmail.com>
2023-02-20 21:12:22 -0500

    Final app.py

--- app.py
+++ app.py
@@ -4,18 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def about_world():  # put application's code here
+def hello_world():  # put application's code here
     return 'Hello BMGT407 from Endalk! This is my about page'
-#helloo
 
 @app.route('/about')
 def about_me():  # put application's code here
     return render_template('about.html')
-
-@app.route('/2')
-def hello_world():  # put application's code here
-    return 'Hello World from Endalk! This is my first page'
-#helloo
 
 @app.route('/hello')
 def hello():  # put application's code here
