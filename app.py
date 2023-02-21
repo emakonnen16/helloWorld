37cf7c8ff38c83d952dfeb48585dedcce691fdd5
endalk <endalkmakonnen@gmail.com>
2023-02-20 21:07:43 -0500

    Adding hello to app.py

--- app.py
+++ app.py
@@ -12,5 +12,14 @@ def about_world():  # put application's code here
 def about_me():  # put application's code here
     return render_template('about.html')
 
+@app.route('/2')
+def hello_world():  # put application's code here
+    return 'Hello World from Endalk! This is my first page'
+#helloo
+
+@app.route('/hello')
+def hello():  # put application's code here
+    return render_template('hello.html')
+
 if __name__ == '__main__':
     app.run()
