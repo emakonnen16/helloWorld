0c8161d12e872f986a9371dba33932581993c5fd
endalk <endalkmakonnen@gmail.com>
2023-03-27 19:48:02 -0400

    updating hello page for about

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,13 @@ def about_me():  # put application's code here
 @app.route('/hello')
 def hello():  # put application's code here
     return render_template('hello.html')
+
+@app.route('/favorite-course', methods=['GET','POST'])
+def hello():  # put application's code here
+    if request.method == "POST":
+        print('List the department of your favorite school: ' + request.form.get('department') + request.form.get('course_no'))
+        return render_template('favorite-course.html')
+    return render_template('favorite-course.html')
 
 if __name__ == '__main__':
     app.run()
